82690d1061698461cd0730bdcba2b8c5b87c46ce
fdamengual <fdamengual@ulp.edu.ar>
2020-12-02 17:33:11 -0300

    Rich text agregado a eventos

--- src/components/paginas/comeventos/FormEve.js
+++ src/components/paginas/comeventos/FormEve.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Consumer } from "../../../context";
 import Msg from "../../utiles/Msg";
-
+import MyEditor from "../subcomponentes/MyEditor";
 /*
     Parámetros:
     id: Id de la Novedad
@@ -26,18 +26,19 @@ class FormEve extends Component {
         dhora: "00:00:00",
         hhora: "00:00:00",
         descripcion: "",
+        descripcionHTML: "",
         costo: 0,
         invita: "",
         organiza: "",
         foto_uno: "default.jpg",
-        foto_dos: "default.jpg"
+        foto_dos: "default.jpg",
       },
       localidades: [],
       msg: {
         visible: false,
         body: "",
-        tipo: 0
-      }
+        tipo: 0,
+      },
     };
     this.setData = this.setData.bind(this);
     this.handleInputChange = this.handleInputChange.bind(this);
@@ -46,6 +47,21 @@ class FormEve extends Component {
     this.saveData = this.saveData.bind(this);
     this.askDelete = this.askDelete.bind(this);
     this.okDelete = this.okDelete.bind(this);
+    this.handlDescripcionHTMLChange = this.handlDescripcionHTMLChange.bind(
+      this
+    );
+    // this.handlDescripcionChange=this.handlDescripcionChange.bind(this);
+  }
+
+  handlDescripcionHTMLChange(desHTML, des) {
+    console.log(desHTML);
+    this.setState({
+      registro: {
+        ...this.state.registro,
+        descripcionHTML: desHTML,
+        descripcion: des,
+      },
+    });
   }
 
   handleImgChange(event) {
@@ -66,8 +82,8 @@ class FormEve extends Component {
       msg: {
         visible: true,
         body: `Seguro de eliminar "${titulo}"`,
-        tipo: 1
-      }
+        tipo: 1,
+      },
     });
   }
 
@@ -77,8 +93,8 @@ class FormEve extends Component {
         msg: {
           visible: false,
           body: "",
-          tipo: 0
-        }
+          tipo: 0,
+        },
       },
       () => {
         this.props.eliminar(this.state.registro.id);
@@ -89,7 +105,7 @@ class FormEve extends Component {
   saveData(event) {
     event.preventDefault();
     const formData = new FormData();
-    Object.keys(this.state.registro).forEach(key =>
+    Object.keys(this.state.registro).forEach((key) =>
       formData.append(key, this.state.registro[key])
     );
     //Imágenes
@@ -115,8 +131,8 @@ class FormEve extends Component {
           msg: {
             tipo: 0,
             visible: true,
-            body: "El tamaño de la primer imágen supera los 4MB."
-          }
+            body: "El tamaño de la primer imágen supera los 4MB.",
+          },
         });
         return false;
       }
@@ -127,8 +143,8 @@ class FormEve extends Component {
           msg: {
             tipo: 0,
             visible: true,
-            body: "El tamaño de la segunda imágen supera los 4MB."
-          }
+            body: "El tamaño de la segunda imágen supera los 4MB.",
+          },
         });
         return false;
       }
@@ -140,33 +156,33 @@ class FormEve extends Component {
       {
         method: "POST",
         headers: {
-          Authorization: token
+          Authorization: token,
         },
-        body: formData
+        body: formData,
       }
     )
-      .then(res => res.json())
+      .then((res) => res.json())
       .then(
-        result => {
+        (result) => {
           if (!result.err) {
             this.setState({
               msg: {
                 tipo: 0,
                 visible: true,
-                body: "Los datos se guardaron correctamente"
-              }
+                body: "Los datos se guardaron correctamente",
+              },
             });
           } else {
             this.setState({
               msg: {
                 tipo: 0,
                 visible: true,
-                body: result.errMsgs
-              }
+                body: result.errMsgs,
+              },
             });
           }
         },
-        error => {
+        (error) => {
           //???
           console.log(error);
         }
@@ -180,8 +196,8 @@ class FormEve extends Component {
     this.setState({
       registro: {
         ...this.state.registro,
-        [name]: value
-      }
+        [name]: value,
+      },
     });
   }
 
@@ -189,8 +205,8 @@ class FormEve extends Component {
     this.setState({
       registro: {
         ...this.state.registro,
-        idlocalidad: event.target.value
-      }
+        idlocalidad: event.target.value,
+      },
     });
   }
 
@@ -199,24 +215,24 @@ class FormEve extends Component {
     this.setState(
       {
         id: this.props.id,
-        localidades: this.props.localidades
+        localidades: this.props.localidades,
       },
       () => {
         fetch(`${process.env.REACT_APP_API_HOST}/evento/${this.state.id}`, {
           method: "GET",
           headers: {
-            Authorization: token
+            Authorization: token,
             //"Content-Type": "application/json"
-          }
+          },
         })
-          .then(res => res.json())
+          .then((res) => res.json())
           .then(
-            result => {
+            (result) => {
               if (!result.err) {
                 if (parseInt(result.data.count, 10) > 0) {
                   this.setState({
                     registro: result.data.registros[0],
-                    loading: false
+                    loading: false,
                   });
                 } else {
                   console.log("No hay registro: " + this.state.id);
@@ -225,18 +241,18 @@ class FormEve extends Component {
                 this.setState({
                   msg: {
                     visible: true,
-                    body: result.errMsg
-                  }
+                    body: result.errMsg,
+                  },
                 });
               }
             },
-            error => {
+            (error) => {
               //???
               this.setState({
                 msg: {
                   visible: true,
-                  body: error
-                }
+                  body: error,
+                },
               });
             }
           );
@@ -255,7 +271,7 @@ class FormEve extends Component {
   }
 
   render() {
-    const localidades = this.state.localidades.map(localidad => {
+    const localidades = this.state.localidades.map((localidad) => {
       return (
         <option key={`loc-${localidad.id}`} value={localidad.id}>
           {localidad.nombre}
@@ -285,13 +301,13 @@ class FormEve extends Component {
                     style={{
                       width: "250px",
                       height: "200px",
-                      objectFit: "cover"
+                      objectFit: "cover",
                     }}
                     src={`${process.env.REACT_APP_API_HOST}/${
                       process.env.REACT_APP_API_DIRECTORY_EVENTOS_FOTOS
                     }/${this.state.registro.foto_uno}`}
                     alt="Foto"
-                    onClick={e => {
+                    onClick={(e) => {
                       document
                         .getElementById(`file-1-${this.state.registro.id}`)
                         .click();
@@ -313,13 +329,13 @@ class FormEve extends Component {
                     style={{
                       width: "250px",
                       height: "200px",
-                      objectFit: "cover"
+                      objectFit: "cover",
                     }}
                     src={`${process.env.REACT_APP_API_HOST}/${
                       process.env.REACT_APP_API_DIRECTORY_EVENTOS_FOTOS
                     }/${this.state.registro.foto_dos}`}
                     alt="Foto"
-                    onClick={e => {
+                    onClick={(e) => {
                       document
                         .getElementById(`file-2-${this.state.registro.id}`)
                         .click();
@@ -416,16 +432,14 @@ class FormEve extends Component {
                     </div>
                   </div>
                   <div className="col-sm-12 col-md-12">
+                    <MyEditor
+                      descripcionHTML={this.handlDescripcionHTMLChange}
+                      contenido={this.state.registro.descripcionHTML}
+                    />
+
                     <div className="form-group">
                       <label htmlFor="descripcion">Descripción</label>
-                      <textarea
-                        rows="5"
-                        name="descripcion"
-                        id="descripcion"
-                        className="form-control"
-                        value={this.state.registro.descripcion}
-                        onChange={this.handleInputChange}
-                      />
+
                     </div>
                   </div>
                   <div className="col">
@@ -522,7 +536,7 @@ class FormEve extends Component {
           okAceptar={this.okDelete}
           okClose={() =>
             this.setState({
-              msg: { ...this.state.msg, visible: false, tipo: 0 }
+              msg: { ...this.state.msg, visible: false, tipo: 0 },
             })
           }
           tipo={this.state.msg.tipo}
